4fddd41c03d28be2c4246569fdd487e394c0628f
jeanramalho <jeanramalho.dev@gmail.com>
2022-11-11 10:32:36 -0300

    Começa configuração do app.js

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+//Importar a controller
+
+
+//Importar rota principal
+
+
+//Importar o Banco de dados SQLite
+
+
+//Utilizando Middlewares
+
+
+//Utilizando Cors para liberar a comunicação externa
+
+
+//Rotas
+
+module.exports = app
