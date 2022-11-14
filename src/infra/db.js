af91f878c47e25449cbef9d97d9812dbb52f0bef
jeanramalho <jeanramalho.dev@gmail.com>
2022-11-14 19:28:39 -0300

    cria a requisicao do sqlite3

--- src/infra/db.js
+++ src/infra/db.js
@@ -2,3 +2,5 @@
     Esse arquivo deve ser executado apenas uma vez para que o banco seja criado e populado
 */
 
+//aquivo responsável por criar a tabela e popular o banco de dados
+const sqlite3 = require('sqlite3').verbose()
