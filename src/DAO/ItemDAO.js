4f696c71b828619159e698db3e39db313ed4e8a2
jeanramalho <jeanramalho.dev@gmail.com>
2022-11-21 13:11:31 -0300

    Ajusta função updateItem

--- src/DAO/ItemDAO.js
+++ src/DAO/ItemDAO.js
@@ -149,7 +149,7 @@ class ItemDAO {
 
 
     //atualiza item
-    async updateUser(id, newitem){
+    async updateItem(id, newitem){
         try {
             const sql = `UPDATE ITEM SET NOME = ?, VALOR = ?, QTD = ?, WHERE ID = ?`
 
